dc1e0e13b8d845b14f00c54fd54a5f2b5212371f
adzzzfarr <md.nur.adzfar@gmail.com>
2024-01-09 18:52:43 +0800

    Added rest timer - BottomDrawer to show 4 rest time options if not resting, otherwise shows countdown timer and buttons to add or decrease 30s

--- lib/pages/performed_workout_page.dart
+++ lib/pages/performed_workout_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bottom_drawer/bottom_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout/data/performed_workout_data.dart';
@@ -20,9 +21,14 @@ class _PerformedWorkoutPageState extends State<PerformedWorkoutPage> {
   final weightController = TextEditingController();
   final setsController = TextEditingController();
   final repsController = TextEditingController();
+  final bottomDrawerController = BottomDrawerController();
 
-  Duration workoutDuration = const Duration();
+  Duration workoutDuration = const Duration(seconds: 0);
   Timer? workoutTimer;
+
+  Duration restDuration = const Duration(seconds: 0);
+  Timer? restTimer;
+  bool isResting = false;
 
   @override
   void initState() {
@@ -36,105 +42,86 @@ class _PerformedWorkoutPageState extends State<PerformedWorkoutPage> {
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(
           title: Text(widget.performedWorkout.name),
+          actions: [
+            MaterialButton(
+              onPressed: () => finishWorkout(),
+              child: const Text('Finish'),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => finishWorkout(),
-          child: const Text('Finish'),
-        ),
-        body: Builder(builder: (context) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: displayWorkoutTimer(),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: value.getNumberOfExercises(
-                      widget.performedWorkout.date,
-                      widget.performedWorkout.name),
-                  itemBuilder: (context, index) => Builder(
-                    builder: (context) => ExerciseTile(
-                      workoutType: 'performed',
-                      exerciseName: value
-                          .getIntendedPerformedWorkout(
-                              widget.performedWorkout.date,
-                              widget.performedWorkout.name)!
-                          .exercises[index]
-                          .name,
-                      setsList: value
-                          .getIntendedPerformedWorkout(
-                              widget.performedWorkout.date,
-                              widget.performedWorkout.name)!
-                          .exercises[index]
-                          .getSetsList(),
-                      isCompleted: value
-                          .getIntendedPerformedWorkout(
-                              widget.performedWorkout.date,
-                              widget.performedWorkout.name)!
-                          .exercises[index]
-                          .isCompleted,
-                      onEditSet: (exerciseName, setNumber) =>
-                          showSetDetailsDialog(
-                        exerciseName,
-                        setNumber,
-                        {
-                          setNumber: value
-                              .getIntendedPerformedWorkout(
-                                  widget.performedWorkout.date,
-                                  widget.performedWorkout.name)!
-                              .exercises[index]
-                              .setWeightReps[setNumber]
-                        },
-                      ),
-                      onCheckboxChanged: (value) {
-                        setState(() {
-                          widget.performedWorkout.exercises[index].isCompleted =
-                              !widget.performedWorkout.exercises[index]
-                                  .isCompleted;
-                        });
-                      },
-                      onTilePressed: null,
-                      onDismissed:
-                          null, // Cannot delete exercises in a performed workout
+        body: Builder(
+          builder: (context) {
+            return Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: displayWorkoutTimer(),
                     ),
-                  ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: value.getNumberOfExercises(
+                            widget.performedWorkout.date,
+                            widget.performedWorkout.name),
+                        itemBuilder: (context, index) => Builder(
+                          builder: (context) => ExerciseTile(
+                            workoutType: 'performed',
+                            exerciseName: value
+                                .getIntendedPerformedWorkout(
+                                    widget.performedWorkout.date,
+                                    widget.performedWorkout.name)!
+                                .exercises[index]
+                                .name,
+                            setsList: value
+                                .getIntendedPerformedWorkout(
+                                    widget.performedWorkout.date,
+                                    widget.performedWorkout.name)!
+                                .exercises[index]
+                                .getSetsList(),
+                            isCompleted: value
+                                .getIntendedPerformedWorkout(
+                                    widget.performedWorkout.date,
+                                    widget.performedWorkout.name)!
+                                .exercises[index]
+                                .isCompleted,
+                            onEditSet: (exerciseName, setNumber) =>
+                                showSetDetailsDialog(
+                              exerciseName,
+                              setNumber,
+                              {
+                                setNumber: value
+                                    .getIntendedPerformedWorkout(
+                                        widget.performedWorkout.date,
+                                        widget.performedWorkout.name)!
+                                    .exercises[index]
+                                    .setWeightReps[setNumber]
+                              },
+                            ),
+                            onCheckboxChanged: (value) {
+                              setState(() {
+                                widget.performedWorkout.exercises[index]
+                                        .isCompleted =
+                                    !widget.performedWorkout.exercises[index]
+                                        .isCompleted;
+                              });
+                            },
+                            onTilePressed: null,
+                            onDismissed:
+                                null, // Cannot delete exercises in a performed workout
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          );
-        }),
+                buildBottomDrawer(context),
+              ],
+            );
+          },
+        ),
       ),
     );
-  }
-
-  Widget displayWorkoutTimer() {
-    String twoDigits(int number) => number.toString().padLeft(2, '0');
-    final minutes = twoDigits(workoutDuration.inMinutes.remainder(60));
-    final seconds = twoDigits(workoutDuration.inSeconds.remainder(60));
-
-    return Text('$minutes:$seconds');
-  }
-
-  void startWorkoutTimer() {
-    workoutTimer = Timer.periodic(
-      const Duration(seconds: 1),
-      (timer) => updateWorkoutDuration(),
-    );
-  }
-
-  void updateWorkoutDuration() {
-    setState(() {
-      final seconds = workoutDuration.inSeconds + 1;
-
-      workoutDuration = Duration(seconds: seconds);
-    });
-  }
-
-  void stopWorkoutTimer() {
-    widget.performedWorkout.duration = workoutDuration.inSeconds;
-    print(widget.performedWorkout.duration);
-    workoutTimer?.cancel();
   }
 
   void showSetDetailsDialog(
@@ -223,5 +210,192 @@ class _PerformedWorkoutPageState extends State<PerformedWorkoutPage> {
       ),
       (route) => false,
     );
+  }
+
+  Widget displayWorkoutTimer() {
+    String twoDigits(int number) => number.toString().padLeft(2, '0');
+    final minutes = twoDigits(workoutDuration.inMinutes.remainder(60));
+    final seconds = twoDigits(workoutDuration.inSeconds.remainder(60));
+
+    return Text('$minutes:$seconds');
+  }
+
+  void startWorkoutTimer() {
+    workoutTimer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) => increaseWorkoutDurationByOneSec(),
+    );
+  }
+
+  void increaseWorkoutDurationByOneSec() {
+    setState(() {
+      final seconds = workoutDuration.inSeconds + 1;
+      workoutDuration = Duration(seconds: seconds);
+    });
+  }
+
+  void stopWorkoutTimer() {
+    widget.performedWorkout.duration = workoutDuration.inSeconds;
+    workoutTimer?.cancel();
+  }
+
+  Widget buildBottomDrawer(BuildContext context) {
+    return BottomDrawer(
+      header: buildBottomDrawerHead(context),
+      body: _buildBottomDrawerBody(context),
+      headerHeight: 60,
+      drawerHeight: 360,
+      controller: bottomDrawerController,
+    );
+  }
+
+  Widget buildBottomDrawerHead(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Column(
+        children: const [
+          Padding(
+              padding: EdgeInsets.only(
+                left: 10.0,
+                right: 10.0,
+                top: 20.0,
+              ),
+              child: Text('Rest')),
+          Spacer(),
+          Divider(
+            height: 1.0,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomDrawerBody(BuildContext context) {
+    if (!isResting) {
+      return SizedBox(
+        width: double.infinity,
+        height: 360,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [displayRestTimeButtons()],
+          ),
+        ),
+      );
+    } else {
+      return SizedBox(
+        width: double.infinity,
+        height: 360,
+        child: Row(
+          children: [
+            displayRestTimer(),
+            displayThirtySecondButtons(),
+          ],
+        ),
+      );
+    }
+  }
+
+  Widget displayRestTimer() {
+    String twoDigits(int number) => number.toString().padLeft(2, '0');
+    final minutes = twoDigits(restDuration.inMinutes.remainder(60));
+    final seconds = twoDigits(restDuration.inSeconds.remainder(60));
+
+    return Text('$minutes:$seconds');
+  }
+
+  Widget displayRestTimeButtons() {
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () => setState(() {
+            restDuration = const Duration(minutes: 1);
+            startRestTimer();
+          }),
+          child: const Text('1:00'),
+        ),
+        MaterialButton(
+          onPressed: () => setState(() {
+            restDuration = const Duration(minutes: 2);
+            startRestTimer();
+          }),
+          child: const Text('2:00'),
+        ),
+        MaterialButton(
+          onPressed: () => setState(() {
+            restDuration = const Duration(minutes: 3);
+            startRestTimer();
+          }),
+          child: const Text('3:00'),
+        ),
+        MaterialButton(
+          onPressed: () => setState(() {
+            restDuration = const Duration(minutes: 4);
+            startRestTimer();
+          }),
+          child: const Text('4:00'),
+        ),
+      ],
+    );
+  }
+
+  Widget displayThirtySecondButtons() {
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () {
+            setState(
+              () {
+                final seconds = restDuration.inSeconds + 30;
+                restDuration = Duration(seconds: seconds);
+              },
+            );
+          },
+          child: const Text('+ 30s'),
+        ),
+        MaterialButton(
+          onPressed: () {
+            setState(
+              () {
+                int seconds = restDuration.inSeconds - 30;
+                if (seconds < 0) {
+                  seconds = 0;
+                  isResting = false;
+                }
+                restDuration = Duration(seconds: seconds);
+              },
+            );
+          },
+          child: const Text('- 30s'),
+        ),
+      ],
+    );
+  }
+
+  void startRestTimer() {
+    restTimer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) => decreaseRestDurationByOneSec(),
+    );
+    isResting = true;
+  }
+
+  void decreaseRestDurationByOneSec() {
+    setState(() {
+      final seconds = restDuration.inSeconds - 1;
+
+      if (seconds < 0) {
+        stopRestTimer();
+      }
+
+      restDuration = Duration(seconds: seconds);
+    });
+  }
+
+  void stopRestTimer() {
+    restDuration = const Duration(seconds: 0);
+    restTimer?.cancel();
+    isResting = false;
+    bottomDrawerController.close();
   }
 }
